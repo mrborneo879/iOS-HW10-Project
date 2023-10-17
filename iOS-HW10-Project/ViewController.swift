6c813cc17695b6fd7c260a9817a25dfd34da2b6d
Alexander Stepanov <alexanderlawyer97@gmail.com>
2023-10-17 22:23:46 +0200

    Revert "Merge branch 'develop'"
    This reverts commit 6b50eb18f19547bf21bef6d775fb49290605a2d6, reversing
    changes made to d9571f1eedbd021fee9031f7ca214d2c28960dc1.

--- iOS-HW10-Project/ViewController.swift
+++ iOS-HW10-Project/ViewController.swift
@@ -1,3 +1,9 @@
+//
+//  ViewController.swift
+//  iOS-HW10-Project
+//
+//  Created by Mac Alexander on 17.10.2023.
+//
 
 import UIKit
 
@@ -5,10 +11,9 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        // Do any additional setup after loading the view.
     }
-    
-    func changeBackgroundColor() {
-        view.backgroundColor = .systemRed
-    }
+
+
 }
 
